77bad9ffd9cb0df0db6167569ddb69dd212804a1
Zackory Erickson <zackory@cmu.edu>
2024-01-29 11:35:14 -0500

    Update stretch_ik.py

--- stretch_python/stretch_ik.py
+++ stretch_python/stretch_ik.py
@@ -6,8 +6,10 @@ import numpy as np
 import ikpy.chain
 import stretch_body.robot
 
+# NOTE before running: `python3 -m pip install ikpy graphviz urdfpy`
+
 target_point = [-0.043, -0.441, 0.654]
-target_orientation = ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi/2)
+target_orientation = ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi/2) # [_, _, yaw]
 pretarget_orientation = ikpy.utils.geometry.rpy_matrix(0.0, 0.0, 0.0)
 
 
@@ -30,11 +32,11 @@ print('Run: \'roslaunch stretch_description display.launch\' to see where the ba
 # Remove unnecessary links/joints
 original_urdf = urdfpy.URDF.load(urdf_path)
 modified_urdf = original_urdf.copy()
-names_of_links_to_remove = ['link_right_wheel', 'link_left_wheel', 'caster_link', 'link_gripper_finger_left', 'link_gripper_fingertip_left', 'link_gripper_finger_right', 'link_gripper_fingertip_right', 'link_head', 'link_head_pan', 'link_head_tilt', 'link_aruco_right_base', 'link_aruco_left_base', 'link_aruco_shoulder', 'link_aruco_top_wrist', 'link_aruco_inner_wrist', 'camera_bottom_screw_frame', 'camera_link', 'camera_depth_frame', 'camera_depth_optical_frame', 'camera_infra1_frame', 'camera_infra1_optical_frame', 'camera_infra2_frame', 'camera_infra2_optical_frame', 'camera_color_frame', 'camera_color_optical_frame', 'camera_accel_frame', 'camera_accel_optical_frame', 'camera_gyro_frame', 'camera_gyro_optical_frame', 'laser', 'respeaker_base']
+names_of_links_to_remove = ['link_right_wheel', 'link_left_wheel', 'caster_link', 'link_gripper_finger_left', 'link_gripper_fingertip_left', 'link_gripper_finger_right', 'link_gripper_fingertip_right', 'link_head', 'link_head_pan', 'link_head_tilt', 'link_aruco_right_base', 'link_aruco_left_base', 'link_aruco_shoulder', 'link_aruco_top_wrist', 'link_aruco_inner_wrist', 'camera_bottom_screw_frame', 'camera_link', 'camera_depth_frame', 'camera_depth_optical_frame', 'camera_infra1_frame', 'camera_infra1_optical_frame', 'camera_infra2_frame', 'camera_infra2_optical_frame', 'camera_color_frame', 'camera_color_optical_frame', 'camera_accel_frame', 'camera_accel_optical_frame', 'camera_gyro_frame', 'camera_gyro_optical_frame', 'laser', 'respeaker_base', 'base_imu']
 links_to_remove = [l for l in modified_urdf._links if l.name in names_of_links_to_remove]
 for lr in links_to_remove:
     modified_urdf._links.remove(lr)
-names_of_joints_to_remove = ['joint_right_wheel', 'joint_left_wheel', 'caster_joint', 'joint_gripper_finger_left', 'joint_gripper_fingertip_left', 'joint_gripper_finger_right', 'joint_gripper_fingertip_right', 'joint_head', 'joint_head_pan', 'joint_head_tilt', 'joint_aruco_right_base', 'joint_aruco_left_base', 'joint_aruco_shoulder', 'joint_aruco_top_wrist', 'joint_aruco_inner_wrist', 'camera_joint', 'camera_link_joint', 'camera_depth_joint', 'camera_depth_optical_joint', 'camera_infra1_joint', 'camera_infra1_optical_joint', 'camera_infra2_joint', 'camera_infra2_optical_joint', 'camera_color_joint', 'camera_color_optical_joint', 'camera_accel_joint', 'camera_accel_optical_joint', 'camera_gyro_joint', 'camera_gyro_optical_joint', 'joint_laser', 'joint_respeaker']
+names_of_joints_to_remove = ['joint_right_wheel', 'joint_left_wheel', 'caster_joint', 'joint_gripper_finger_left', 'joint_gripper_fingertip_left', 'joint_gripper_finger_right', 'joint_gripper_fingertip_right', 'joint_head', 'joint_head_pan', 'joint_head_tilt', 'joint_aruco_right_base', 'joint_aruco_left_base', 'joint_aruco_shoulder', 'joint_aruco_top_wrist', 'joint_aruco_inner_wrist', 'camera_joint', 'camera_link_joint', 'camera_depth_joint', 'camera_depth_optical_joint', 'camera_infra1_joint', 'camera_infra1_optical_joint', 'camera_infra2_joint', 'camera_infra2_optical_joint', 'camera_color_joint', 'camera_color_optical_joint', 'camera_accel_joint', 'camera_accel_optical_joint', 'camera_gyro_joint', 'camera_gyro_optical_joint', 'joint_laser', 'joint_respeaker', 'joint_base_imu']
 joints_to_remove = [l for l in modified_urdf._joints if l.name in names_of_joints_to_remove]
 for jr in joints_to_remove:
     modified_urdf._joints.remove(jr)
@@ -67,6 +69,11 @@ print(f"num joints: {len(modified_urdf.joints)}")
 
 iktuturdf_path = "/tmp/iktutorial/stretch.urdf"
 modified_urdf.save(iktuturdf_path)
+
+# tree = ikpy.urdf.utils.get_urdf_tree(iktuturdf_path, "base_link")[0]
+# print(tree)
+# print(ikpy.urdf.utils.get_urdf_tree(iktuturdf_path, "base_link")[1])
+# tree.view()
 
 chain = ikpy.chain.Chain.from_urdf_file(iktuturdf_path)
 
